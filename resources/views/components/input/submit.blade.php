e97a85a0585203c4e3338426db2ed3c46de859bc
Dylan <dylanschutte10@gmail.com>
2024-07-28 13:02:17 +0200

    Update submit.blade.php

--- resources/views/components/input/submit.blade.php
+++ resources/views/components/input/submit.blade.php
@@ -1,10 +1,6 @@
-@props([
-    'label' => 'Save Changes',
-    'icon' => 'bi bi-floppy',
-])
 
 
-<button type="submit" id="submit" {{ $attributes->merge(['class' => 'btn btn-primary w-100']) }}>
+<button type="submit" id="submit" class="{{ $classList }}">
     <i class="{{ $icon }}"></i>
     {{ $label }}
 </button>
