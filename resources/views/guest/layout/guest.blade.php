ac3c7f7dcb20af84015d849b60289165c34905b1
Dylan <dylanschutte10@gmail.com>
2024-07-28 13:37:04 +0200

    Added Icons to Guest Layout

--- resources/views/guest/layout/guest.blade.php
+++ resources/views/guest/layout/guest.blade.php
@@ -1,25 +1,28 @@
 <!DOCTYPE html>
 <html lang="en" data-bs-theme="{{ $theme }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
 
-    @vite([
-        'resources/sass/guest.scss', 
-        'resources/js/guest.js',
-    ])
+    @vite(['resources/sass/guest.scss', 'resources/js/guest.js'])
 
+    @foreach ($stylesheets as $stylesheet)
+        <link href="{{ $stylesheet }}" rel="stylesheet">
+    @endforeach
 
 </head>
+
 <body>
 
     <x-symlink-spinner />
 
-    @include("symlink::guest.includes.navbar")
+    @include('symlink::guest.includes.navbar')
     {{ $slot }}
 
 
 </body>
+
 </html>
