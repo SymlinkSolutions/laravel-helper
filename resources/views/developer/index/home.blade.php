0fda4e43534fe9d02c11c532dfe79f28900c895b
Dylan <dylanschutte10@gmail.com>
2024-08-03 10:40:03 +0200

    Dev Screen layout update

--- resources/views/developer/index/home.blade.php
+++ resources/views/developer/index/home.blade.php
@@ -8,7 +8,7 @@
             <div class="row">
                 <p class="fs-3">General Settings</p>
             </div>
-            <div class="row">
+            <div class="row mb-3">
                 <div class="col-auto">
                     {!! Form::submit('Save Changes') !!}
                 </div>
@@ -21,25 +21,31 @@
 
             <div class="row">
                 <div class="col-4">
-
-                    {!! Form::itext('app_name', 'App Name', $app_name) !!}
-                    {!! Form::itext('primary_font_family', 'Primary Font Family', $primary_font_family) !!}
-                    {!! Form::itextarea('font_primary', 'Font Primary', $font_primary) !!}
-                    {!! Form::itext('secondary_font_family', 'Secondary Font Family', $secondary_font_family) !!}
-                    {!! Form::itextarea('font_secondary', 'Font Secondary', $font_secondary) !!}
-                    
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-auto">
+                                    <h4 class="card-title">ENV</h4>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                {!! Form::itext('app_name', 'App Name', $app_name) !!}
+                            </div>
+                        </div>
+                    </div>
 
                 </div>
                 <div class="col-4">
                     <div class="card">
                         <div class="card-body">
                             <div class="row">
-                                <div class="col-auto">
+                                <div class="col">
                                     <h4 class="card-title">Bootstrap Colors</h4>
                                 </div>
                                 <div class="col-auto">
                                     {!! Html::button('Reset Colors', route('dev.reset.colors'), [
-                                        '.btn-secondary' => true,
+                                        '.btn-danger' => true,
                                     ]) !!}
                                 </div>
                             </div>
@@ -53,6 +59,25 @@
                                 {!! Form::icolorpicker('bs_info', 'info', $bs_info, []) !!}
                                 {!! Form::icolorpicker('bs_light', 'light', $bs_light, []) !!}
                                 {!! Form::icolorpicker('bs_dark', 'dark', $bs_dark, []) !!}
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="col-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-auto">
+                                    <h4 class="card-title">Fonts</h4>
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                {!! Form::itext('primary_font_family', 'Primary Font Family', $primary_font_family) !!}
+                                {!! Form::itextarea('font_primary', 'Font Primary', $font_primary) !!}
+                                {!! Form::itext('secondary_font_family', 'Secondary Font Family', $secondary_font_family) !!}
+                                {!! Form::itextarea('font_secondary', 'Font Secondary', $font_secondary) !!}
                             </div>
                         </div>
                     </div>
