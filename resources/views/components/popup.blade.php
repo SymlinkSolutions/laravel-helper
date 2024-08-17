e1b46db86fecc9ced7de3e22064606109087e296
Dylan <dylanschutte10@gmail.com>
2024-08-17 11:07:36 +0200

    Access middleware bug fix

---
+++ resources/views/components/popup.blade.php
@@ -0,0 +1,11 @@
+
+
+<div class="modal fade modal-dialog modal-dialog-scrollable modal-dialog-centered" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body">
+                This is the modal body
+            </div>
+        </div>
+    </div>
+</div>
