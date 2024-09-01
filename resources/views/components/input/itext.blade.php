1738c55a2ec51dfdb660c299d2993195fcd704f4
Dylan <dylanschutte10@gmail.com>
2024-09-01 10:48:56 +0200

    Attributes bug fix

--- resources/views/components/input/itext.blade.php
+++ resources/views/components/input/itext.blade.php
@@ -1,12 +1,13 @@
 <div class="mt-2 mb-2">
     <div class="form-floating has-validation">
-        <input 
+        <input
             class='form-control @if($errors && $errors->has($id)) is-invalid @endif'
-            type="{{ $type }}" 
-            id="{{ $id }}" 
-            name="{{ $id }}" 
-            placeholder="{{ $label }}" 
+            type="{{ $type }}"
+            id="{{ $id }}"
+            name="{{ $id }}"
+            placeholder="{{ $label }}"
             value="{{ $value }}"
+            {{ $attributes }}
         />
         <label for="{{ $id }}">{{ $label }}</label>
         @if($errors && $errors->has($id))
