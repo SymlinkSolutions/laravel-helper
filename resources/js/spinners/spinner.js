7251a4989cc2082ae7fb55a45949f87f5475438f
Dylan <dylanschutte10@gmail.com>
2024-09-15 10:37:26 +0200

    Fixed loading screen from staying when its not suppose to

--- resources/js/spinners/spinner.js
+++ resources/js/spinners/spinner.js
@@ -1,9 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    window.addEventListener('beforeunload', function(){
+    window.addEventListener('beforeunload', function() {
         document.getElementById('spinner-overlay').style.display = 'flex';
     });
 
-    window.addEventListener('load', function(){
+    window.addEventListener('load', function() {
         document.getElementById('spinner-overlay').style.display = 'none';
     });
+
+    function checkLoadingStatus() {
+        if (document.readyState === 'complete') {
+            document.getElementById('spinner-overlay').style.display = 'none';
+            clearInterval(loadingCheckInterval);
+        }
+    }
+
+    var loadingCheckInterval = setInterval(checkLoadingStatus, 1000);
 });
