1df7345ba906892d4c97ea68ce2ff9e681bce30a
Dylan <dylanschutte10@gmail.com>
2024-09-18 16:05:56 +0200

    Bug fix

--- src/Models/BankingDetails.php
+++ src/Models/BankingDetails.php
@@ -10,6 +10,18 @@ use Symlink\LaravelHelper\Traits\HasCustomUuid;
 class BankingDetails extends Model {
     use HasFactory, HasCustomUuid;
 
+    protected $fillable = [
+        "uuid",
+        "account_number",
+        "bank_name",
+        "branch_code",
+        "user_id",
+    ];
+
+    protected $casts = [
+        "account_number" => 'encrypted',
+    ];
+
     public function user(){
         return $this->belongsTo(User::class);
     }
