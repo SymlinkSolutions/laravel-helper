4c001df576a53c082582f3d3333cb9e2e2fdbdcb
Dylan <dylanschutte10@gmail.com>
2024-10-02 17:21:44 +0200

    Spinner update

--- resources/views/components/spinner.blade.php
+++ resources/views/components/spinner.blade.php
@@ -1,5 +1,7 @@
 
-
+@php
+    
+@endphp
 <div id="spinner-overlay">
 @switch($name)
     @case('spinner_one')
@@ -8,11 +10,11 @@
             <div class="orbit"></div>
         </div>
         @break
+    @case('spinner_two')
+        <div id="spinner_two"></div>
+    @break
     @default
-        <div id="spinner_one" class="fulfilling-bouncing-circle-spinner">
-            <div class="circle"></div>
-            <div class="orbit"></div>
-        </div>
+        <div id="spinner_two"></div>
     @endswitch
         
 </div>
